13594a49782de35581b451f2f4e2515203b27912
tamanetcs <tamanetcs@gmail.com>
2025-07-24 17:22:15 -0600

    Add markdown-it and sanitize-html for RSS feed content processing

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,6 +1,10 @@
 import rss from '@astrojs/rss'
 import type { APIRoute } from 'astro'
 import { getCollection } from 'astro:content'
+import MarkdownIt from 'markdown-it'
+import sanitizeHtml from 'sanitize-html'
+
+const parser = new MarkdownIt()
 
 export const GET: APIRoute = async ({ params, request, site }) => {
     const blogPost = await getCollection('blog')
@@ -12,16 +16,30 @@ export const GET: APIRoute = async ({ params, request, site }) => {
         title: 'Abraham’s Blog',
         // `<description>` field in output xml
         description: 'A single blog about me...',
+        xmlns: {
+            media: 'http://search.yahoo.com/mrss/'
+        },
         // Pull in your project "site" from the endpoint context
         // https://docs.astro.build/en/reference/api-reference/#site
         site: site ?? '',
         // Array of `<item>`s in output xml
         // See "Generating items" section for examples using content collections and glob imports
-        items: blogPost.map(({ data, slug }) => ({
+        items: blogPost.map(({ data, slug, body }) => ({
             title: data.title,
             pubDate: data.date,
             description: data.description,
-            link: `posts/${slug}`
+            link: `posts/${slug}`,
+            content: sanitizeHtml(parser.render(body), {
+                allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+            }),
+
+            customData: `<media:content
+                type="image/${data.image.format === 'jpg' ? 'jpeg' : 'png'}"
+                width="${data.image.width}"
+                height="${data.image.height}"
+                medium="image"
+                url="${site + data.image.src}" />
+            `
         })),
         // (optional) inject custom xml
         customData: `<language>es-mx</language>`
